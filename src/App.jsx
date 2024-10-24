47c3d14cd6f447312112f1486ffffd1324217cac
Estefanía Aguas <estefaniaaguas@gmail.com>
2024-10-23 20:42:05 -0500

    chore: folder structure and contexts are created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import { routes } from "./utils/routes";
+import Home from "./routes/Home";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-red-200">
-      Hello world!
-    </h1>
+    <>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path={routes.base} element={<Home />} />
+        <Route path={routes.home} element={<Home />} />
+        {/* <Route path={routes.details} element={<Detail />} />
+        <Route path={routes.favs} element={<Favs />} />
+        <Route path={routes.notFound} element={<NotFound />} /> */}
+      </Routes>
+      {/* <Footer /> */}
+    </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './styles/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from 'react-router-dom';
+import ContextProvider from './contexts/ContextProvider.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </BrowserRouter>
+);
